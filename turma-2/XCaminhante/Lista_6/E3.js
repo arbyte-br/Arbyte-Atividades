e133e26d9cd29801d8cb26c351167c69bda7dd97
XCaminhante <caminhante@i2pmail.org>
2020-06-20 15:12:36 -0300

    Lista 6 (incompleta)

---
+++ turma-2/XCaminhante/Lista_6/E3.js
@@ -0,0 +1,16 @@
+const rs = require('readline-sync')
+function apenas1a10 (n) { return !Number.isNaN(n) && 1<=n && n<=10 }
+function pedirNumeros () {
+  var opts = {limit:apenas1a10, limitMessage:'Apenas números entre 1 e 10'}
+  return Array(5).fill(0).map( (v,i)=> rs.question(`Número #${i+1}: `,opts) )
+}
+var nomeA = rs.question('Nome A: ')
+var numsA = pedirNumeros()
+var nomeB = rs.question('Nome B: ')
+var numsB = pedirNumeros()
+function distintos (a) {
+  return a.filter( (e,i,v)=> v.indexOf(e,i+1)==-1 )
+}
+console.log(`${nomeA}: ${numsA}`)
+console.log(`${nomeB}: ${numsB}`)
+console.log(`Números distintos que apareceram: ${distintos(numsA.concat(numsB))}`)
