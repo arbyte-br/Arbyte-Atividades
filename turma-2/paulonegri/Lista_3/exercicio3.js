473e49a76c258bbbac6305662087464a56451cd4
PauloNegri <Paulo_negrii@hotmail.com>
2020-06-23 22:18:16 -0300

    exercicio3 resolvido!

---
+++ turma-2/paulonegri/Lista_3/exercicio3.js
@@ -0,0 +1,11 @@
+//Escreva um algoritmo que imprima todos os números ímpares de 1até 100
+
+let rs = require("readline-sync")
+
+let numero = 1
+console.log(numero)
+while(numero < 99){
+    numero = numero + 2
+    console.log(numero)
+    
+}
