2f34281614cb9d8667efe028bdccf4f5e78c657f
brunobosca <66393978+brunobosca@users.noreply.github.com>
2020-06-23 22:14:30 -0300

    Adicionando exercícios da lista_3
    Exercícios lista3 - While

---
+++ turma-2/brunobosca/Lista_3/E01.js
@@ -0,0 +1,8 @@
+let readlineSync = require('readline-sync')
+let numeroEntrada = readlineSync.questionInt('Digite um número de entrada ')
+
+while (numeroEntrada > 0) {
+    console.log('BIP BIP')
+    numeroEntrada--
+}
+    
---
+++ turma-2/brunobosca/Lista_3/E03.js
@@ -0,0 +1,8 @@
+
+let readlineSync = require('readline-sync')
+let impares = 1;
+    while(impares <= 100) {
+        console.log(impares);
+        impares = impares + 2;
+    }
+    console.log("Fim!");
