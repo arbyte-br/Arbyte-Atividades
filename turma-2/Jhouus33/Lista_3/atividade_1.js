4be86b3ae73f4a0b43ef18d355041bd64240973a
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-06-24 20:17:43 -0300

    atividade 1 ok

---
+++ turma-2/Jhouus33/Lista_3/atividade_1.js
@@ -0,0 +1,10 @@
+let rs = require('readline-sync')
+
+let contador = 0
+let repeticoes = rs.questionInt("Me diga número:   \n")
+
+while (contador !== repeticoes) {
+    console.log('BIP BIP')
+    contador = contador + 1
+        
+}
