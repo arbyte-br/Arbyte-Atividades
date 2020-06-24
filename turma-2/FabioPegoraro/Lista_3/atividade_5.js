0984bea59635089bcebc40e599347cb5d433f3ca
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-06-24 16:39:16 -0300

    Resultado Atividade 5

---
+++ turma-2/FabioPegoraro/Lista_3/atividade_5.js
@@ -0,0 +1,8 @@
+let rs = require("readline-sync")
+
+let respostaUsuario = rs.questionInt("Insira 0 para sair ou qualquer outra tecla para continuar.\n") 
+let respostacerta = 0
+
+while(respostaUsuario !== respostacerta){
+        respostaUsuario = rs.questionInt("Insira 0 para sair ou qualquer outra tecla para continuar.\n")            
+}
