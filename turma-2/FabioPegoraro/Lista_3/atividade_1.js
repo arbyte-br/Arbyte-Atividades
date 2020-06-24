2cfd23790b3a97bd6f003ad35d7857e64f73845f
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-06-23 21:41:38 -0300

    Resultado atividade 1

---
+++ turma-2/FabioPegoraro/Lista_3/atividade_1.js
@@ -0,0 +1,11 @@
+let rs = require("readline-sync")
+
+let respostaUsuario = rs.questionInt("Digite um numero:\n")
+let contador= 0
+
+console.log("==================================")
+while (contador < respostaUsuario) {
+    console.log("BIP BIP")
+    contador++
+}
+console.log("==================================")
