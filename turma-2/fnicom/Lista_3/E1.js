3bb67668bf783e75755a26d079cdf836c8a356e2
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-06-23 22:11:09 -0300

    Add files via upload

---
+++ turma-2/fnicom/Lista_3/E1.js
@@ -0,0 +1,27 @@
+let rs = require('readline-sync');
+let numero = rs.questionInt('insira um numero ');
+
+let contador = 0;
+
+while(true) {
+    console.log('BIP BIP');
+    if(contador === numero - 1){
+        break;
+    }
+    contador = contador + 1;
+  }
+
+// let rs = require ('readline-sync');
+
+//let num = rs.questionInt('Digite um numero');
+
+//CONT = 0;   
+//while (CONT < num){
+  //  console.log ('BIP BIP');
+  //  num = num - 1;
+//}
+  
+
+
+
+
---
+++ turma-2/fnicom/Lista_3/E2.js
@@ -0,0 +1,13 @@
+let rs = require('readline-sync');
+let num1 = rs.questionInt('insira um numero ');
+let num2 = rs.questionInt('insira um numero ');
+
+while(true) {
+  num1 + num2 
+console.log(num1 + num2)
+  
+break;
+  }
+ 
+
+
