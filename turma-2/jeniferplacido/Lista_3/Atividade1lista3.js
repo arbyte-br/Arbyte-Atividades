e2666a7f391f848bb1622bd77a9ebb75c66d061d
Jenifer Placido <jenifer.sdti@gmail.com>
2020-06-23 22:21:20 -0300

    Atividade1lista3

---
+++ turma-2/jeniferplacido/Lista_3/Atividade1lista3.js
@@ -0,0 +1,11 @@
+let rs = require ('readline-sync')
+
+
+let contador = 3
+
+let repeticoes =rs.questionInt ("Digite um numero:\n");
+
+while (contador !== repeticoes){
+    console.log ("BIP BIP BIP")
+    contador = contador + 1
+}
