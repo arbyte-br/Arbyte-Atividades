cc0d5ad4b50dade9c452b99fd5dd24a35b9ca3e7
raphahf6 <raphaellima6@gmail.com>
2020-06-24 04:00:59 -0300

    Sobe atividade while lista 3

---
+++ turma-2/Raphahf6/Lista_3/main.js
@@ -0,0 +1,10 @@
+let rs = require('readline-sync')
+
+let inputUser = rs.questionInt('Digite um numero: ')
+let counter = 0
+
+while(counter < inputUser) {
+    console.log("BIP BIP")
+
+    counter++
+}
