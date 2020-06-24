a2a12053c3abf8b9c14ae99fb0286ff2193af505
PauloNegri <Paulo_negrii@hotmail.com>
2020-06-24 20:00:33 -0300

    exercicio2 finalizado!

---
+++ turma-2/paulonegri/Lista_5/exercicio1.js
@@ -0,0 +1,10 @@
+//Faça um algoritmo que receba uma entrada do usuário e caso encontre ​uma vogal​ transforme ela 
+//em maiúscula (obrigatório usar FOR)EX: abelhaO algoritmo imprimeAbElhA
+
+let rs=require("readline-sync")
+let palavra= rs.question("Digite qualquer palavra!\n")
+
+for(palavra !== "exit"){
+    console.log(palavra.)
+
+}    toUpperCase()
---
+++ turma-2/paulonegri/Lista_5/exercicio2.js
@@ -0,0 +1,23 @@
+//Faça um programa que leia dois números, o primeiro é o valor inicialde um contador, e o segundo é
+// o valor final do contador (verifique se o valor inicial fornecido é inferior ao valor final). 
+//Usando o comando For, escreva na tela uma contagem que comece no primeiro número lido, escreva os 
+//números seguintes colocando apenas um número em cada nova linha da tela, até chegar ao valor final 
+//indicado.
+
+let rs= require("readline-sync")
+console.log("Seja bem vindo!")
+let numero1 = rs.questionInt("Digite o valor inicial = ")
+let numero2 = rs.questionInt("Digite o valor final = ")
+
+    
+
+if(numero1 >= numero2){
+    
+    console.log(" O numero inicial deve ser menor que o numero final.")
+    
+}else if(numero1 < numero2){
+    for(console.log(); numero1 <= numero2; numero1++){
+        console.log(numero1)
+    }
+    
+}
