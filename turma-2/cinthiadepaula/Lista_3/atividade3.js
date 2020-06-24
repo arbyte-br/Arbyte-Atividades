0198debc9f65dba352746edd775f49dc7c7bee11
Cinthia de Paula <66394231+cinthiadepaula@users.noreply.github.com>
2020-06-24 17:19:02 -0300

    Add files via upload
    atividades listra 3

---
+++ turma-2/cinthiadepaula/Lista_3/atividade3.js
@@ -0,0 +1,12 @@
+let rs = require ('readline-sync');
+
+
+let CONT 
+
+CONT = 1;   
+while (CONT < 100){
+    console.log(CONT)
+    CONT=CONT+2;
+}
+
+
---
+++ turma-2/cinthiadepaula/Lista_3/atividade4.js
@@ -0,0 +1,13 @@
+let rs = require ('readline-sync');
+
+let numero = rs.questionInt('Digite um numero ');
+
+let CONT
+
+CONT = 0;   
+while (CONT <= numero){
+    console.log(CONT)
+    CONT=CONT+2;
+}
+
+
