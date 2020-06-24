7002b92229fb71c8597d56aadd8204c35185f507
PauloNegri <Paulo_negrii@hotmail.com>
2020-06-24 13:09:47 -0300

    sexto exercicio finalizado!

---
+++ turma-2/paulonegri/Lista_3/exercicio7.js
@@ -0,0 +1,3 @@
+//Faça um programa que calcule a potência (potenciação) de um número.Para isso ele deve receber 
+//dois números de entrada: o primeiro número é a base dapotência, o segundo número será a
+//potência
