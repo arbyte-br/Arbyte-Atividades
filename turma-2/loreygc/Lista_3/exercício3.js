3480b6e947ad07e7ae867d14b02be42c87f5f40d
Lorena <loosgc@gmail.com>
2020-06-24 19:11:00 -0300

    update exercício 3

--- turma-2/loreygc/Lista_3/exercício3.js
+++ turma-2/loreygc/Lista_3/exercício3.js
@@ -1,3 +1,13 @@
-let rs= require ('readline-sync')
 
-let 
+let Num = 1, msg = "";
+while (Num <= 100) {
+    if (Num % 2 === 1) {
+        if (Num === 100){
+            msg += Num + ". ";
+        }else{
+            msg += Num + ", ";
+        }
+    }
+    Num++;
+}
+console.log(msg);
