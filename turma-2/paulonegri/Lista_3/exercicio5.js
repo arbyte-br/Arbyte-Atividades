33ee9631ef54f9520216c518d70346177a0c3cd9
PauloNegri <Paulo_negrii@hotmail.com>
2020-06-23 22:50:25 -0300

    exercicio cinco terminado!

---
+++ turma-2/paulonegri/Lista_3/exercicio5.js
@@ -0,0 +1,6 @@
+let rs = require("readline-sync")
+let perguntaUsuario = rs.question(" Insira 0 para sair ou qualquer outra tecla para continuar.")
+
+while(perguntaUsuario !== "0"){
+    perguntaUsuario = rs.question(" Insira 0 para sair ou qualquer outra tecla para continuar.")
+}
