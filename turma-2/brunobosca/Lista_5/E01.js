6800ccf807bd9b60bdf984966c69cca606434abe
brunobosca <66393978+brunobosca@users.noreply.github.com>
2020-06-24 21:50:11 -0300

    ExerciciosLista05
    Lista_05 Exercicios FOR

---
+++ turma-2/brunobosca/Lista_5/E01.js
@@ -0,0 +1,20 @@
+let readlineSync = require('readline-sync');
+
+let palavra = readlineSync.question('Insira uma palavra: ');
+
+let tamanho = palavra.length;
+
+let palavraFinal = [];
+
+for (let i = 0; i < tamanho; i++) {
+    let comp = palavra.charAt(i);
+    if (comp == 'a' || comp == 'e' || comp == 'i' || comp == 'o' || comp == 'u'){
+        comp = comp.toUpperCase();
+        palavraFinal.push(comp);
+    } else {
+        palavraFinal.push(comp);
+    }
+}
+
+console.log(palavraFinal.join(''));
+
