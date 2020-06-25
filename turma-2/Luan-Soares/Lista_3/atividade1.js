b64219c240bfe2acc2b15ad6df6c07c7a6d95986
Luan Felipe <luan_soares163@hotmail.com>
2020-06-24 21:28:41 -0300

    fazendo as atividades do exercicio 3

---
+++ turma-2/Luan-Soares/Lista_3/atividade1.js
@@ -0,0 +1,10 @@
+let rs = require('readline-sync')
+
+let entradaUsuario = rs.questionInt("Insira um numero: ")
+
+let contador = 0
+
+while (contador < entradaUsuario) {
+    console.log("BIP BIP")
+    contador++
+}
---
+++ turma-2/Luan-Soares/Lista_3/atividade2.js
@@ -0,0 +1,15 @@
+let rs = require('readline-sync')
+
+let primeiroNumero = rs.questionFloat('Digite um número: ') // 3
+
+let segundoNumero = rs.questionInt('Digite outro número: ') // 2
+
+let contador = 0
+
+let resultado = 0
+
+while(contador < segundoNumero) {
+    resultado = resultado + primeiroNumero
+    contador++
+}
+console.log("O resultado da multiplicação é: " + resultado)
