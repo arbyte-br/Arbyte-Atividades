63dbe94137739dadd1a970558f4d23c7347d28a1
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-06-24 21:36:33 -0300

    atividades 5 ok com For

--- turma-2/Jhouus33/Lista_3/atividade_5.js
+++ turma-2/Jhouus33/Lista_3/atividade_5.js
@@ -1,8 +1,5 @@
 let rs = require('readline-sync')
 
-let respostaUsuario 
-
-while (respostaUsuario !== 0) {
-    respostaUsuario = rs.questionInt ('Digite 0 para sair ou qualque número para continuar: \n')
-    
+for ( let respostaUsuario; respostaUsuario !== 0; respostaUsuario) {
+    respostaUsuario = rs.questionInt ('Digite 0 para sair ou qualque número para continuar: \n')   
 }
