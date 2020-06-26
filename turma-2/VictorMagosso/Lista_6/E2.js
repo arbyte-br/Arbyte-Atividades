3c7aba630e5cdf821a15fe934d16acbc8f897998
Victor Magosso <aplicativosmagosso@gmail.com>
2020-06-25 22:13:17 -0300

    Exercícios Array

---
+++ turma-2/VictorMagosso/Lista_6/E2.js
@@ -0,0 +1,28 @@
+let rs = require('readline-sync')
+
+let jogadorA = rs.question('Qual o seu nome? Voce sera o jogar A: ')
+let jogadorB = rs.question('Qual o seu nome? Voce sera o jogar B: ')
+
+let numerosJogadorA = []
+let numerosJogadorB = []
+
+for(let i = 0; i < 5; i++){
+    let nA = rs.questionInt(`${jogadorA}, digite 5 numeros: `)
+    numerosJogadorA.push(nA)
+}
+console.log("===================")
+
+for(let i = 0; i < 5; i++){
+    let nB = rs.questionInt(`${jogadorB}, digite 5 numeros: `)
+    numerosJogadorB.push(nB)
+}
+console.log(`${jogadorA}, seus números são: ${numerosJogadorA}`)
+console.log("===================")
+console.log(`${jogadorB}, seus números são: ${numerosJogadorB}`)
+
+
+for(let i = 0; i < 5; i++){ 
+    if(numerosJogadorA.indexOf(numerosJogadorB[i]) == -1){
+        console.log(`Os números diferentes são: ${numerosJogadorB[i]}`)
+    }
+}
