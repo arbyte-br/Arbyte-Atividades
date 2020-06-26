d97cbacf76f7c5ece78335122aa0a11a9b6ee3c5
João Victor <victorps.joao@gmail.com>
2020-06-25 21:53:24 -0300

    atividades obrigatórios da lista 5 resolvidas

---
+++ turma-2/joaovictorps/Lista_5/atividade2.js
@@ -0,0 +1,12 @@
+let rs = require('readline-sync')
+
+let num1 = rs.questionInt('Digite um número : ')
+let num2 = rs.questionInt('Digite outro número maior que o anterior: ')
+
+if( num1 < num2) {
+    for(num1; num1 <= num2; num1++) {
+        console.log(num1) 
+    }
+}   else {
+    console.log('Você o segundo número menor que o primeiro.')
+}
---
+++ turma-2/joaovictorps/Lista_5/atividade3.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 1000 ;i++) {
+    if(i%5===0){
+        console.log(i)
+    }
+}
