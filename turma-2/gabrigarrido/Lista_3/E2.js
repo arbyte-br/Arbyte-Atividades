757286f11c714ae9729a4ea6a6315d4ff84a34a8
Gabriel <47401345+gabrigarrido@users.noreply.github.com>
2020-06-26 13:12:21 -0300

    Update E2.js

--- turma-2/gabrigarrido/Lista_3/E2.js
+++ turma-2/gabrigarrido/Lista_3/E2.js
@@ -3,13 +3,13 @@ var rs = require('readline-sync');
 var primeiroNumero = rs.questionFloat("Digite um número: ")
 var segundoNumero = rs.questionFloat("Digite um segundo número: ")
 
-// var contador = 0
-// var resultado = 0
+var contador = 0
+var resultado = 0
 
-// while(contador < segundoNumero){
-//     resultado = resultado+primeiroNumero;
-//     contador++
-// }
+while(contador < segundoNumero){
+    resultado = resultado+primeiroNumero;
+    contador++
+}
 
-// console.log(resultado)
+ console.log(resultado)
 
