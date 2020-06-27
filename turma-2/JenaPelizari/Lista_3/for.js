ba68889292305ea0ecfe9e1cb28a75050eef6970
Natasha Pelizari <65843895+JenaPelizari@users.noreply.github.com>
2020-06-26 22:07:47 -0300

    pasta errada

--- turma-2/JenaPelizari/Lista_3/for.js
+++
@@ -1,7 +0,0 @@
-let rs = require('readline-sync')
-
-for (let respostaUsuario; respostaUsuario !== 0; null) {
-    respostaUsuario = rs.questionInt ('Digite 0 para sair ou qualque número para continuar: \n')
-    
-}
-
