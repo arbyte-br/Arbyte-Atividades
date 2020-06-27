8bf59b051604859bae682ab463ad069f9faf9a8b
rafahmarinho <rafael.marinho96@hotmail.com>
2020-06-26 22:39:27 -0300

    pasta FOR removida

--- turma-2/rafahmarinho/Lista_3/FOR/exercicio_1.js
+++
@@ -1,9 +0,0 @@
-let rs = require ('readline-sync')
-
-let repeat = rs.questionInt (console.log('Digite um número de repetições de BIP:'))
-
-let cont = 0
-
-for (cont = 0 ;cont !== repeat; cont ++ ) {
-    console.log ('BIP BIP')    
-}
--- turma-2/rafahmarinho/Lista_3/FOR/exercicio_3.js
+++
@@ -1,7 +0,0 @@
-let limite = 100
-
-for (contador = 0; contador <= limite; contador ++) {
-    if (contador % 2 !== 0) {
-    console.log (contador)
-}
-}
