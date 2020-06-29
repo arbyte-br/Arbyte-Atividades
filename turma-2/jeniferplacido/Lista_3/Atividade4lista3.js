8c9ee6a7e6ab62a3ff8dc9c0cf01692de86d4c62
Jenifer Placido <jenifer.sdti@gmail.com>
2020-06-28 22:25:31 -0300

    Atividade4lista3

--- turma-2/jeniferplacido/Lista_3/Atividade4lista3.js
+++ turma-2/jeniferplacido/Lista_3/Atividade4lista3.js
@@ -1,5 +1,5 @@
 let rs = require('readline-sync')
-let limite = rs.questionInt("Digite um número: ")
+let limite = rs.questionInt(console.log ("Digite um número: "))
 for (contador = 1; contador <= limite; contador ++) {
     if (contador % 2 === 0) {
 
