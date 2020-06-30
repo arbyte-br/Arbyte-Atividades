68dbd170a80a6b2aff4621a4813a784eaa01f9c5
PauloNegri <Paulo_negrii@hotmail.com>
2020-06-30 12:35:55 -0300

    exercicio1 finalizado

--- turma-2/paulonegri/Lista_5/exercicio1.js
+++ turma-2/paulonegri/Lista_5/exercicio1.js
@@ -1,21 +1,23 @@
 //Faça um algoritmo que receba uma entrada do usuário e caso encontre ​uma vogal​ transforme ela 
 //em maiúscula (obrigatório usar FOR)EX: abelhaO algoritmo imprimeAbElhA
-
-let rs=require("readline-sync")
+let rs= require("readline-sync")
 let pg
 
 for(let pg; pg !== "nao"; null){
+
+    let palavra = rs.question("Digite qualquer palavra: ").toLowerCase()
+    let vogais = "aeiou"
+    let tamanho = palavra.length
+
+    for(let i = 0; i < tamanho; i++) {
+        let vogal = palavra.charAt(i)
     
-let palavra = rs.question("Digite qualquer palavra!\n").toLowerCase()
-let tamanho = palavra.length
-    
-    for(let i=0;i<tamanho;i++){
-        if(palavra.charAt(i) === "a"||palavra.charAt(i) === "e"||palavra.charAt(i) === "i"||palavra.charAt(i) === "o"||palavra.charAt(i) === "u"){
-            console.log(palavra.charAt(i).toUpperCase().slice(palavra))
-        }else{
-            console.log(palavra.charAt(i))
+        if(vogais.includes(vogal)) {
+            let antesDavogal = palavra.slice(0, i)
+            let depoisDaVogal = palavra.slice(i + 1)
+            palavra = antesDavogal + vogal.toUpperCase() + depoisDaVogal
         }
-    }
-    pg = rs.question("Voce gostaria de ver as vogais de uma nova em palavra maiusculas?(Responda com sim ou nao)").toLowerCase()
-  
+    }console.log(palavra)
+    pg = rs.question("Gostaria de fazer novamente com outra palavra?(sim ou nao) ").toLowerCase()
 }
+
