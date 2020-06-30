9959c4c0845b197f8e91c18fabaa7a1508c04ba7
malesimatheus <66446789+malesimatheus@users.noreply.github.com>
2020-06-30 16:26:56 -0300

    deletando arquivo local errado

--- turma-2/malesimatheus/E5.js
+++
@@ -1,9 +0,0 @@
-let rs = require('readline-sync');
-
-let nome = rs.question('Insira um nome: ');
-
-if (nome.charAt(2)) {
-console.log('A terceira letra é ' + nome.charAt(2));
-} else {
-    console.log('Não tem a terceira letra');
-}
