a7a77bee495677c4594b5f37e113023c6e01ee7c
Paulo Henrique Santos Gabriel <66282539+Yleus@users.noreply.github.com>
2020-06-30 07:39:08 -0300

    Delete E1.js

--- turma-2/Yleus/Lista_1/E1.js
+++
@@ -1,9 +0,0 @@
-let rs = require('readline-sync')
-
-let entradaUsuario = rs.questionInt("Insira um número: ")
-let contador = 0
-
-while (contador < entradaUsuario) {
-    console.log("BIP BIP")
-    contador++
-}
