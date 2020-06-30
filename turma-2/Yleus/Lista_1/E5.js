1acf7ca136baf1fc05e215b4efd5d816072685ef
Paulo Henrique Santos Gabriel <66282539+Yleus@users.noreply.github.com>
2020-06-30 07:39:35 -0300

    Delete E5.js

--- turma-2/Yleus/Lista_1/E5.js
+++
@@ -1,7 +0,0 @@
-let rs = require('readline-sync')
-
-let respostaUsuario
-
-while (respostaUsuario !== 0) {
-    respostaUsuario =rs.questionInt("Insira 0 para sair ou qualquer outro número para continuar")
-}
