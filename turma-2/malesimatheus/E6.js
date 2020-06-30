21698996d528c972ff114b005fa7ea1e480b52d3
malesimatheus <66446789+malesimatheus@users.noreply.github.com>
2020-06-30 16:26:45 -0300

    deletando arquivo local errado

--- turma-2/malesimatheus/E6.js
+++
@@ -1,9 +0,0 @@
-let rs = require('readline-sync');
-
-let palavra = rs.question('Insira uma palavra: ');
-
-if(palavra.endsWith('al')) {
-    console.log(palavra);
-} else {
-    console.log('Palavra nao identificada!!!');
-}
