8e6d5145cf69a249799bd61f11185713071df9f1
cfrinka <66394921+cfrinka@users.noreply.github.com>
2020-07-01 18:42:54 -0300

    exercicios lista 5
    exercicios lista 5 for

---
+++ turma-2/cfrinka/Lista_5/E2.js
@@ -0,0 +1,11 @@
+let rs = require('readline-sync');
+let n1 = rs.questionInt('Insira um número: ');
+let n2 = rs.questionInt('Insira um número: ');
+
+
+    for (let i = n1 -1; i < n2; n1++){
+    i++
+    console.log(i)      
+}
+    
+console.log('final da contagem')
