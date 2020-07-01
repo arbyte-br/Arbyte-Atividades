2b1dd2d1e614e777f736b2194f9fdff58a0c1faf
JeffBassP <66394065+JeffBassP@users.noreply.github.com>
2020-06-30 22:02:53 -0300

    Atividades do dia 29/06

---
+++ turma-2/JeffBassP/Lista_7/Atividade_3.js
@@ -0,0 +1,17 @@
+let rs = require('readline-sync');
+
+function idade(ano, mes, dia)
+{
+    ano = ano*365;
+    mes = mes*30;
+
+    resultado = ano+mes+dia;
+    return resultado;
+}
+
+let a = rs.question('Digite quantos anos: ');
+let m = rs.question('Digite quantos meses: ');
+let d = rs.question('Digite quantos dias: ');
+
+console.log(`Voce ja viveu ${idade(a,m,d)} dias!!`);
+
