442805e8a14ffc4cde93e52c5f6476ea24cbd366
Edimar Soares Ladeia <66395697+Ladeia2@users.noreply.github.com>
2020-07-01 21:19:23 -0300

    Adicionando exercício da lista_6
    Exercício Inner Join

---
+++ turma-2/Ladeia2/Lista_6/E1.js
@@ -0,0 +1,49 @@
+// (OBRIGATÓRIO) 
+// OK - Faça um algoritmo que receba o nome de dois jogadores 
+// OK - e após isso peça 5 números 
+// OK - de 1 a 10 para cada jogador. 
+// OK - Primeiro para o jogador A e depois para o jogador B. 
+// OK - Após a escolha de cada jogador o algoritmo deve apresentar 
+// quais foram os números iguais que o jogador A e B colocaram.
+// EX: 
+// Jogador A: 1, 2, 3, 4, 5
+// Jogador B: 1, 2, 3, 8, 7
+// Algoritmo imprime: 1,2,3 
+let rs = require('readline-sync')
+let tamanhoDasListas = 5
+let nomeJogador1 = rs.question('Digite o nome do jogador 1: ')
+let nomeJogador2 = rs.question('Digite o nome do jogador 2: ')
+console.clear()
+let listaJogador1 = []
+for (let i = 0; i < tamanhoDasListas; i++) {
+    let respostaJogador = -1
+    while (respostaJogador < 0 || respostaJogador > 10) {
+        respostaJogador = rs.questionInt(`Digite um número entre 0 e 10, ${nomeJogador1}: `)
+    }
+    listaJogador1.push(respostaJogador)
+}
+console.clear()
+let listaJogador2 = []
+for (let i = 0; i < tamanhoDasListas; i++) {
+    let respostaJogador = -1
+    while (respostaJogador < 0 || respostaJogador > 10) {
+        respostaJogador = rs.questionInt(`Digite um número entre 0 e 10, ${nomeJogador2}: `)
+    }
+    listaJogador2.push(respostaJogador)
+}
+console.clear()                                      
+let numerosRepetidos = []
+for(let i = 0; i < tamanhoDasListas; i++) {    
+    let itemLista1 = listaJogador1[i]
+    for(let j = 0; j < tamanhoDasListas; j++) {
+        let itemLista2 = listaJogador2[j]
+        let achouNumeroRepetido = itemLista1 === itemLista2
+        if (achouNumeroRepetido) {
+            let numeroNaoEstaInclusoNaLista = !numerosRepetidos.includes(itemLista1)
+            if (numeroNaoEstaInclusoNaLista) {
+                numerosRepetidos.push(itemLista1)
+            }
+        }
+    }                                                   
+}        
+console.log('Estes são os número repetidos: ', numerosRepetidos.join(', '))
