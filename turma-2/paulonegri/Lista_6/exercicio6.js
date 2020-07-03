1017bde2ed6bb441cae6ac69a04a6c2dfb9e9887
PauloNegri <Paulo_negrii@hotmail.com>
2020-07-02 21:17:30 -0300

    exercicio 6 finalizado!

---
+++ turma-2/paulonegri/Lista_6/exercicio6.js
@@ -0,0 +1,4 @@
+//Faça um programa que receba uma palavra e verifica se ela termina
+//com “al” se termina com “al” então imprime a palavra se não imprime “palavra não
+//identificada”.
+//Utilize o método endsWith()
