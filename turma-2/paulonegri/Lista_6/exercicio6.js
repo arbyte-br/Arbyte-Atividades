9e66ade9febb74025aa8dcd74576a2415d01b6ea
PauloNegri <Paulo_negrii@hotmail.com>
2020-07-02 21:47:38 -0300

    exercicio 6 finalizado!

--- turma-2/paulonegri/Lista_6/exercicio6.js
+++ turma-2/paulonegri/Lista_6/exercicio6.js
@@ -2,3 +2,18 @@
 //com “al” se termina com “al” então imprime a palavra se não imprime “palavra não
 //identificada”.
 //Utilize o método endsWith()
+
+let rs = require('readline-sync')
+let palavra = rs.question(`Digite uma palavra aqui: `)
+
+palavra.endsWith('al') ? console.log(`Palavra identificada -> ${palavra}`) 
+: console.log(`palavra não identificada`)
+console.clear()
+
+let i =rs.question(`Gostaria de tentar novamente?(Responda com sim ou nao)`).toLowerCase()
+for(i; i !== 'nao'; null){
+    palavra = rs.question(`Digite uma palavra aqui: `)
+    palavra.endsWith('al') ? console.log(`Palavra identificada -> ${palavra}`) 
+    : console.log(`palavra não identificada`)
+    i =rs.question(`Gostaria de tentar novamente?(Responda com sim ou nao)`).toLowerCase()
+}
