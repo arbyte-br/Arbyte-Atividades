44ed7dc4457f525a840ca0ddd6047afc3958d100
PauloNegri <Paulo_negrii@hotmail.com>
2020-07-04 20:14:49 -0300

    exercicio corrigido!

--- turma-2/paulonegri/Lista_1/exercicio5.js
+++ turma-2/paulonegri/Lista_1/exercicio5.js
@@ -7,17 +7,17 @@ let valor2 = rs.questionInt('Digite o segundo valor: ')
 let valor3 = rs.questionInt('Digite o terceiro valor: ')
 
 if(valor1 > valor2 && valor1 > valor3 && valor2 > valor3){
-    console.log(`Valores em ordem crescente \n>${valor1}\n>${valor2}\n>${valor3}`)
-}else if(valor1 > valor2 && valor1 > valor3 && valor2 < valor3){
-    console.log(`Valores em ordem crescente \n>${valor1}\n>${valor3}\n>${valor2}`)
-}else if(valor1 > valor2 && valor1 < valor3 && valor2 < valor3){
-    console.log(`Valores em ordem crescente \n>${valor3}\n>${valor1}\n>${valor2}`)
-}else if(valor1 < valor2 && valor1 < valor3 && valor2 < valor3){
     console.log(`Valores em ordem crescente \n>${valor3}\n>${valor2}\n>${valor1}`)
-}else if(valor1 < valor2 && valor1 > valor3 && valor2 > valor3){
-    console.log(`Valores em ordem crescente \n>${valor2}\n>${valor1}\n>${valor3}`)
-}else if(valor1 < valor2 && valor1 < valor3 && valor2 > valor3){
+}else if(valor1 > valor2 && valor1 > valor3 && valor2 < valor3){
     console.log(`Valores em ordem crescente \n>${valor2}\n>${valor3}\n>${valor1}`)
+}else if(valor1 > valor2 && valor1 < valor3 && valor2 < valor3){
+    console.log(`Valores em ordem crescente \n>${valor2}\n>${valor1}\n>${valor3}`)
+}else if(valor1 < valor2 && valor1 < valor3 && valor2 < valor3){
+    console.log(`Valores em ordem crescente \n>${valor1}\n>${valor2}\n>${valor3}`)
+}else if(valor1 < valor2 && valor1 > valor3 && valor2 > valor3){
+    console.log(`Valores em ordem crescente \n>${valor3}\n>${valor1}\n>${valor2}`)
+}else if(valor1 < valor2 && valor1 < valor3 && valor2 > valor3){
+    console.log(`Valores em ordem crescente \n>${valor1}\n>${valor3}\n>${valor2}`)
 }else{
     console.log(`Não é permitido repetir valores iguais!`)
 }
