5eb4f62b8b78dabdad2998d9e47f15c705cb85cf
raphahf6 <raphaellima6@gmail.com>
2020-07-03 22:25:22 -0300

    sobe atividades

--- turma-2/Raphahf6/Lista_7/atividade8.js
+++ turma-2/Raphahf6/Lista_7/atividade8.js
@@ -6,18 +6,28 @@ let PlayerB = rs.question('Digite o nome do segundo jogador: ')
 let numberA = rs.questionInt(`${PlayerA} digite um numero: `)
 let numberB = rs.questionInt(`${PlayerB} digite outro numero: `)
 
-let numeroCorreto = 100
 
-function verificaNumero(n1, n2){
-    let resposta1 = n1 - numeroCorreto
-    let resposta2 = n2 - numeroCorreto
 
-    if(resposta1 > resposta2){
+function verificaNumero(n1, n2) {
+    let numeroCorreto = 40
+    let resposta1 = 0
+    let resposta2 = 0
+
+    if (numeroCorreto < n1) {
+         resposta1 = n1 - numeroCorreto
+    } else if (numeroCorreto < n2) {
+         resposta2 = n2 - numeroCorreto
+    } else {
+         resposta1 = numeroCorreto - n1
+         resposta2 = numeroCorreto - n2
+    }
+
+    if (resposta1 > resposta2) {
         console.log(`Parabens ${PlayerA} seu numero foi oque chegou mais proximo de ${numeroCorreto}`)
-    } else if(resposta1 < resposta2){
+    } else if (resposta1 < resposta2) {
         console.log(`Parabens ${PlayerB} seu numero foi oque chegou mais proximo de ${numeroCorreto}`)
 
-    } 
+    }
 }
 
 verificaNumero(numberA, numberB)
