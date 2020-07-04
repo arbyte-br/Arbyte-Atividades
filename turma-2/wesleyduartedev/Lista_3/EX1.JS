f3f6f37f487bc8759c4ff269c2dd98d2efdaa22f
Wesley Duarte <66394028+wesleyduartedev@users.noreply.github.com>
2020-07-04 12:11:48 -0300

    Add files via upload
    Exercicios editados

---
+++ turma-2/wesleyduartedev/Lista_3/EX1.JS
@@ -0,0 +1,24 @@
+//(OBRIGATÓRIO) Faça um programa que recebe um número de entrada e repita a
+///mensagem “BIP BIP” de acordo com o número inserido.
+//EX: usuário inseriu 3
+//O programa imprime:
+//BIP BIP
+//BIP BIP
+//BIP BIP
+
+let rs = require('readline-sync');
+
+let numero = rs.questionInt('Insira o numero de BEP:');
+
+ let bep = 1;
+ let contador = numero;
+
+while( contador > 0){
+    
+     console.log( 'BEP');
+     contador = contador - 1;
+
+
+    
+}
+
