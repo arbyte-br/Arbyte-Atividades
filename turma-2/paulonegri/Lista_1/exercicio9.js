1795aa5635a54e623bad984ad23931ad3841f61e
PauloNegri <Paulo_negrii@hotmail.com>
2020-07-05 11:08:47 -0300

    exercicio atualizado!

--- turma-2/paulonegri/Lista_1/exercicio9.js
+++ turma-2/paulonegri/Lista_1/exercicio9.js
@@ -5,7 +5,7 @@ Senão estiver na faixa, imprimir
 Você digitou um numero fora da faixa entre 100 e 200.*/
 
 let rs = require('readline-sync')
-let numero = rs.questionInt('Digite um número: ')
+let numero = rs.questionInt('Digite um numero: ')
 
 numero>100 && numero<200 ? console.log(`Voce digitou um numero entre 100 e 200.`)
 : console.log(`Voce digitou um numero fora da faixa entre 100 e 200.`)
