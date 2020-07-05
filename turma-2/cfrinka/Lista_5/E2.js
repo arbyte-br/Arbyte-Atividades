f0a1afa4156f1b79668375292df19eea72e80283
cfrinka <66394921+cfrinka@users.noreply.github.com>
2020-07-05 12:18:28 -0300

    exercicios completos
    arrumei os exercicios com falha

--- turma-2/cfrinka/Lista_5/E2.js
+++ turma-2/cfrinka/Lista_5/E2.js
@@ -3,8 +3,11 @@ let n1 = rs.questionInt('Insira um número: ');
 let n2 = rs.questionInt('Insira um número: ');
 
 
-    for (let i = n1 -1; i < n2; n1++){
-    i++
+    for (let i = n1; i <= n2; i++){
+    console.log(i)      
+}
+
+for (let i = n2; i <= n1; i++){
     console.log(i)      
 }
     
