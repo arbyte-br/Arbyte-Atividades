faf756842a90b26ed6e7d2acc9296d8f4ba7db4d
cfrinka <66394921+cfrinka@users.noreply.github.com>
2020-07-05 12:17:00 -0300

    exercicios completos
    arrumei os exercicios que estavam com falhas

--- turma-2/cfrinka/Lista_6/E1.js
+++ turma-2/cfrinka/Lista_6/E1.js
@@ -1,33 +1,24 @@
 let rs = require('readline-sync');
-
-
-let jogador1 = rs.question('Digite seu nome: ');
-let n1 = rs.questionInt('Digite um número de 1 a 10: ');
-let n2 = rs.questionInt('Digite outro número de 1 a 10: ');
-let n3 = rs.questionInt('Digite outro número de 1 a 10: ');
-let n4 = rs.questionInt('Digite outro número de 1 a 10: ');
-let n5 = rs.questionInt('Digite outro número de 1 a 10: ');
-let arrayjogador1 = [n1,n2,n3,n4,n5];
-let jogador2 = rs.question('Digite seu nome: ');
-let n6 = rs.questionInt('Digite um número de 1 a 10: ');
-let n7 = rs.questionInt('Digite outro número de 1 a 10: ');
-let n8 = rs.questionInt('Digite outro número de 1 a 10: ');
-let n9 = rs.questionInt('Digite outro número de 1 a 10: ');
-let n10 = rs.questionInt('Digite outro número de 1 a 10: ');
-let arrayjogador2 = [n6,n7,n8,n9,n10];
+let jogador1 = rs.question('Qual o seu nome? Você será o Jogador 1 ')
+let jogador2 = rs.question('Qual o seu nome? Você será o jogador 2 ')
+let arrayjogador1 = [] ;
+let arrayjogador2 = [];
 let comum = [];
 
+for (let i = 0; i < 5 ; i++){
+    let nA = rs.questionInt(` ${jogador1}, insira um número: `);
+    arrayjogador1.push(nA)
+}
+console.log('=============')
+
+for (let i = 0; i < 5; i++){
+    let nB = rs.questionInt(`${jogador2}, insira um número: `);
+    arrayjogador2.push(nB)
+}
 for (let i = 0; i <= 4; i++){
     if (arrayjogador1.indexOf(arrayjogador2[i]) > -1) {
         comum.push(arrayjogador2[i])
                 
     }
 }
-console.log(comum)
-
-
-
-
-
-
-
+console.log(`Os número em comum são: ${comum}`)
