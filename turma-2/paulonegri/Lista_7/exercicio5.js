97359ff56b8c85ad7b23a9c11e0b355b879a1d0d
PauloNegri <Paulo_negrii@hotmail.com>
2020-07-06 14:35:32 -0300

    exercicio resolvido!

---
+++ turma-2/paulonegri/Lista_7/exercicio5.js
@@ -0,0 +1,13 @@
+/*Escreva uma aplicação em JS que receba dois números do usuário e
+retorne a soma dos mesmos. A soma deve ser feita em uma função.*/
+
+let rs = require('readline-sync')
+let primerioNumero = rs.questionInt(`Digite o primeiro numero: `)
+let segundoNumero = rs.questionInt(`Digite o primeiro numero: `)
+
+
+function soma (){
+
+    return primerioNumero + segundoNumero
+}
+console.log(`${primerioNumero} + ${segundoNumero} = ${soma()}`)
