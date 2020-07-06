ca0eb1931a49e02c45d8d2b0c8cf0b99ad52a91e
Cinthia de Paula <66394231+cinthiadepaula@users.noreply.github.com>
2020-07-06 15:10:06 -0300

    Update at1.js
    corrigindo

--- turma-2/cinthiadepaula/Lista_6/at1.js
+++ turma-2/cinthiadepaula/Lista_6/at1.js
@@ -1,33 +1,39 @@
-let rs = require('readline-sync');
+let rs = require('readline-sync')
+let tamanhoDasListas = 5
+let nomeJogador1 = rs.question('Digite o nome do jogador 1: ')
+let nomeJogador2 = rs.question('Digite o nome do jogador 2: ')
 
-let jogadorA = rs.question('Insira o nome do jogador 1 ');
-let jogadorB = rs.question('Insira o nome do jogador 2 ');
-
-
-let numJogadorA = [];
-let numJogadorB = [];
-let respostasIguais = [];
-
-cont=5;
-
-
-for (i = 0; i < cont; i++) {
-  numJogadorA[i] = rs.question(`${jogadorA} digite o ${i} numero `);
-  
+console.clear()
+let listaJogador1 = []
+for (let i = 0; i < tamanhoDasListas; i++) {
+    let respostaJogador = -1
+    while (respostaJogador < 0 || respostaJogador > 10) {
+        respostaJogador = rs.questionInt(`Digite um número entre 0 e 10, ${nomeJogador1}: `)
+    }
+    listaJogador1.push(respostaJogador)
 }
-
-
-for (i = 0; i < cont; i++) {
-  numJogadorB[i] = rs.question(`${jogadorB} digite o ${i} numero `);
-  
+console.clear()
+let listaJogador2 = []
+for (let i = 0; i < tamanhoDasListas; i++) {
+    let respostaJogador = -1
+    while (respostaJogador < 0 || respostaJogador > 10) {
+        respostaJogador = rs.questionInt(`Digite um número entre 0 e 10, ${nomeJogador2}: `)
+    }
+    listaJogador2.push(respostaJogador)
 }
-
-for(i=0; i < cont; i++)
-{
-let condition = numJogadorB.indexOf(numJogadorA[i]) >-1;
-if(condition && respostasIguais.indexOf(numJogadorA[i])<0){
-  respostasIguais.push(numJogadorA[i]);
-}
-}
-
-console.log(respostasIguais);
+console.clear()                                      
+let numerosRepetidos = []
+for(let i = 0; i < tamanhoDasListas; i++) {    
+    let itemLista1 = listaJogador1[i]
+    for(let j = 0; j < tamanhoDasListas; j++) {
+        let itemLista2 = listaJogador2[j]
+        let achouNumeroRepetido = itemLista1 === itemLista2
+        if (achouNumeroRepetido) {
+            let numeroNaoEstaInclusoNaLista = !numerosRepetidos.includes(itemLista1)
+            if (numeroNaoEstaInclusoNaLista) {
+                numerosRepetidos.push(itemLista1)
+            }
+        }
+    }                                                   
+}        
+console.log('Estes são os número repetidos: ', numerosRepetidos.join(', '))
