1db3fa2bca63decf1a856f7e92675e03a44358ef
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-07-06 14:38:11 -0300

    Resultado Atividade 6

---
+++ turma-2/FabioPegoraro/Lista_7/atividade_6.js
@@ -0,0 +1,16 @@
+
+
+let rs = require("readline-sync")
+
+
+let num = rs.questionFloat("Digite um numero entre 1 e 10:\n")
+
+function detectarnumero(){
+    if (num > 1 && num <= 10){
+    return console.log("Número OK");
+    }else {
+        console.log ("O número deve ser de 1 a 10!")
+    }
+}
+
+let resultado = detectarnumero ()
