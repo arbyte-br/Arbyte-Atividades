358a8382266b24a3d16e7a94bb7272d59555722a
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-07-07 18:55:24 -0300

    Modificações nos exerciocios

--- turma-2/FabioPegoraro/Lista_4/atividade_1.js
+++ turma-2/FabioPegoraro/Lista_4/atividade_1.js
@@ -1,9 +1,9 @@
 //Escreva um programa que pedirá para o usuário dois números entre 1 e 100. OK
 //O programa deve, então, iterar de um em um do menor pro maior, sempre imprimindo o número na tela do usuário. OK
-//Entretanto, caso o número seja divisível por 7, o programa deve ao invés disso imprimir a palavra “Ping”. 
-//Caso o número seja divisível por 5, o programa deve imprimir “Pong”. 
-//Caso o número seja divisível por 7 e 5, o programa deveria imprimir a mensagem “Ping Pong”. 
-//Dica: Neste exercício vocês talvez precisem fazer uso do recurso “user.questionInt()” que transforma o conteúdo de um input do usuário de string para inteiro.
+//Entretanto, caso o número seja divisível por 7, o programa deve ao invés disso imprimir a palavra “Ping”. OK
+//Caso o número seja divisível por 5, o programa deve imprimir “Pong”. OK
+//Caso o número seja divisível por 7 e 5, o programa deveria imprimir a mensagem “Ping Pong”. OK
+//Dica: Neste exercício vocês talvez precisem fazer uso do recurso “user.questionInt()” que transforma o conteúdo de um input do usuário de string para inteiro. OK
 
 let rs = require("readline-sync")
 // Pedindo numeros p/ usuario
--- turma-2/FabioPegoraro/Lista_4/atividade_2.js
+++ turma-2/FabioPegoraro/Lista_4/atividade_2.js
@@ -4,21 +4,36 @@
 //Dica: Neste exercício você pode precisar de ajuda do operador MÓDULO (%). 
 //Você também pode precisar criar mais de um loop. 
  
-//descobrindo se o numero é par ou impar
+//descobrindo se o numero PRIMO
+function ehPrimo(numero) {
+    let contador = 2
+    let achouDivisorAlemDeleMesmoEUm = false
+    if (numero === 1) {
+        return false
+    }
+    while (contador < numero) {
+        if (numero % contador === 0) {
+            achouDivisorAlemDeleMesmoEUm = true
+        }
+        contador++
+    }
+    if (achouDivisorAlemDeleMesmoEUm) {
+        return false
+    } else {
+        return true
+    }
+}
+//====================================
+//descobrindo se o numero é PAR ou IMPAR
 let contador = 1
 let fimContador = 50
-
-
-while ( contador <= fimContador){
-    let numPar = contador % 2
-    if (numPar === 0){
-        console.log(contador, "Par")
-        if (contador / contador && contador / 1){
-            console.log ("Primo")
-        }
-    }else{
-        console.log(contador, "Impar")
-    }    
-       contador++
+while (contador <= fimContador) {
+    let sobraDaDivisao = contador % 2
+    if (sobraDaDivisao === 0) {
+        console.log(contador, 'PAR', ehPrimo(contador) ? 'É Primo' : 'Não é primo')
+    } else {
+        console.log(contador, 'IMPAR', ehPrimo(contador) ? 'É Primo' : 'Não é primo')
+    }
+    contador++
 }
-//======================
+//====================================
