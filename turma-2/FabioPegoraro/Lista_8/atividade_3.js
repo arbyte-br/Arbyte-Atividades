9af51a992765d17522564cacd56ae72b0f3d4d9e
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-07-08 20:59:31 -0300

    Resultado dos exercicios Lista 8

---
+++ turma-2/FabioPegoraro/Lista_8/atividade_3.js
@@ -0,0 +1,12 @@
+//Crie um programa que crie um objeto chamado "carro" com as propriedades "marca", "cor" e "valor" 
+//e imprima o objeto na tela. (Você pode decidir o valor de "marca", "cor" e "valor", 
+//não é necessário pedir para o usuário) 
+
+let carro = {
+    marca: "Fiat",
+    cor: "Vermelho",
+    valor: 12000.00,
+}
+
+
+console.log(carro)
---
+++ turma-2/FabioPegoraro/Lista_8/atividade_5.js
@@ -0,0 +1,16 @@
+//Crie um programa que pergunte o nome do usuário e peça para este usuário digitar 3 números, 
+//o programa deve imprimir, no formato de um objeto, o nome e os números informados.
+
+let rs = require("readline-sync")
+
+let nome = rs.question("Qual o seu nome?\n")
+let n1 = rs.question("Digite um nuemro?")
+let n2 = rs.question("Digite um nuemro?")
+let n3 = rs.question("Digite um nuemro?")
+
+let usuario = {
+    nomeUsuario: nome,
+    numeros: [n1, n2, n3],
+}
+
+console.log(usuario)
