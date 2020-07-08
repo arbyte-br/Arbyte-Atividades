5273bfdaa144700f814c41689be322b715c79b01
Cinthia de Paula <66394231+cinthiadepaula@users.noreply.github.com>
2020-07-07 22:03:40 -0300

    atualizando
    atualizando

--- turma-2/cinthiadepaula/Lista_8/at1.js
+++ turma-2/cinthiadepaula/Lista_8/at1.js
@@ -1,20 +1,17 @@
 let rs = require ('readline-sync');
+let fs = require ('fs');
 
 let cor = rs.question('Digite a cor do carro: ');
 let modelo = rs.question('Digite o modelo do carro: ');
 let marca = rs.question('Digite a marca do carro: ');
 
-let carro = {};
-carro.cor = cor;
-carro.modelo = modelo;
-carro.marca = marca;
+let carro = {
+corCarro: cor,
+modeloCarro: modelo,
+marcaCarro: marca
+};
 
-console.log(`A cor do carro e : ${carro.cor}`);
-console.log(`O modelo do carro e : ${carro.modelo}`);
-console.log(`A marca do carro e : ${carro.marca}`);
-
-let carroJSON = JSON.stringify(carro)
-const fs = require ('fs');
 let caminhoJson = 'carro.json';
+let carroJSON = JSON.stringify(carro);
 
-fs.writeFileSync(caminhoJson, caminhoJson);
+fs.writeFileSync(caminhoJson, carroJSON);
