c2cabb1eb9278e311a0488170377af07e22b04f9
DaniSanttos <66403367+DaniSanttos@users.noreply.github.com>
2020-07-08 21:39:16 -0300

    Adicionando Exercícios da lista 8

---
+++ turma-2/DaniSanttos/Lista_8/E1.js
@@ -0,0 +1,11 @@
+let xicara = {
+    cheia: true,
+    cor: 'branca'
+}
+console.log(xicara.cheia)
+
+// if (xicara.cheia === quantidadeDentroDaXicara){
+//     console.log('Xícara cheia')
+// }else{
+//     console.log('A xicara ainda não está cheia.')
+// }
