3a595d7bee21ee5e4654ddfe229d3218b9a7ee5f
raphahf6 <raphaellima6@gmail.com>
2020-07-09 20:26:03 -0300

    sobe atividade 1.2 e 2.2

---
+++ turma-2/Raphahf6/Lista_8/atividade2.2.js
@@ -0,0 +1,34 @@
+// (OBRIGATÓRIO) Crie um programa que:
+// a. Peça o nome de 1 usuário;
+// b. Peça a sua data de nascimento no formato DD/MM/AAAA (ex: 24/12/1990);
+// c. Com base na data de nascimento, calcule sua idade (a data de hoje você
+// mesmo pode definir em uma variável);
+// d. Imprima na tela a idade do usuário.
+// e. Pare a execução.
+
+let rs = require('readline-sync')
+
+let userName = rs.question('Digite um nome: ')
+let diaNascimento = rs.questionInt('Digite o dia do seu nascimento: ')
+let mesNascimento = rs.questionInt('Digite o numero do mês do seu nascimento: ')
+let anoNascimento = rs.questionInt('Digite o ano do seu nascimento: ')
+
+let anoAtual = 2020
+let mesAtual = 07
+let dataNascimento = [`${diaNascimento}/${mesNascimento}/${anoNascimento}`]
+
+let idade = anoAtual - anoNascimento
+
+if(mesAtual < mesNascimento){
+    idade = idade -1
+}
+
+
+let user = {
+    nome : userName,
+    nascimento: dataNascimento,
+    idade : idade
+}
+
+console.log(user)
+
