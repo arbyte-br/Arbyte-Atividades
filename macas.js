4aec20a2772c2e72d98954f7e2de8bb842445703
jralan91 <66448731+jralan91@users.noreply.github.com>
2020-07-08 21:00:55 -0300

    Add files via upload

---
+++ macas.js
@@ -0,0 +1,11 @@
+let rs = require('readline-sync');
+ let maca = rs.question('Quantas macas voce deseja? \n');
+
+ if (maca>=12){
+    console.log('R$(maca * 0,25)')
+ }
+ //else{
+   //  let precount = 0,3
+  //
+
+ //console.log('Total ')
