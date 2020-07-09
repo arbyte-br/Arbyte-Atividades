dfb7bc1298c5b02db98b3ca5c7a2cb277575b6b7
PauloNegri <Paulo_negrii@hotmail.com>
2020-07-09 19:03:32 -0300

    exercicio resolvido!

---
+++ turma-2/paulonegri/Lista_8/exercicio3.js
@@ -0,0 +1,7 @@
+/*Crie um programa que crie um objeto chamado "carro" com as
+propriedades "marca", "cor" e "valor" e imprima o objeto na tela. (Você pode decidir o
+valor de "marca", "cor" e "valor", não é necessário pedir para o usuário)*/
+
+let carro = {marca: `volkswagen`, modelo: `Fusca`, cor: `Preto`, valor: `R$15.000,00`}
+
+console.log(carro)
