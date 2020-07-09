3326b5ccdd7eaa39aca2f73de0470a5f6848ee92
brunobosca <66393978+brunobosca@users.noreply.github.com>
2020-07-08 21:11:02 -0300

    Exercicios lista_9
    Adicionando exercícios lista_9

---
+++ turma-2/brunobosca/Lista_9/e05.js
@@ -0,0 +1,13 @@
+
+let ageCalculator = require('age-calculator');
+
+let {AgeFromDateString, AgeFromDate} = require('age-calculator')
+
+
+let ageFromDate = new AgeFromDate(new Date(1993, 0, 9)).age
+console.log("Sua idade é ", ageFromDate)
+
+
+
+
+
