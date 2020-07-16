f816fdd11119fa74871940255d0f253303bc1295
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-07-16 01:01:35 -0300

    Lista 5 - Atividade 3 ok

---
+++ turma-2/Jhouus33/Lista_5/atividade_3.js
@@ -0,0 +1,9 @@
+// Faça um programa que escreva na tela os números múltiplos de 5
+// entre 0 e 1000, usando um comando For. Não utilize nenhum IF/THEN neste programa,
+// apenas o comando For.
+
+let num = 1000
+for(let i = 0; i <= num; i = i + 5){
+    console.log(i)
+} 
+
