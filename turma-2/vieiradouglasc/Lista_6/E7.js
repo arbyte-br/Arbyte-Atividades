d6866ff8b4e0806bcb7d709f239ecda0892f30bc
vieiradouglasc <50721517+vieiradouglasc@users.noreply.github.com>
2020-07-15 22:03:12 -0300

    Termino Exercício 7 - Lista 6

--- turma-2/vieiradouglasc/Lista_6/E7.js
+++ turma-2/vieiradouglasc/Lista_6/E7.js
@@ -2,6 +2,8 @@ let rs = require('readline-sync')
 
 let palavra = rs.question('Insira uma palavra: ')
 
-if (palavra.includes('p') && palavra.includes('b')) {
-
+if (palavra.includes('mp') && palavra.includes('mb')) {
+    console.log('Parabéns, sua gramática está correta')
+} else {
+    console.log('Antes de “p” ou “b” devemos usar M')
 }
