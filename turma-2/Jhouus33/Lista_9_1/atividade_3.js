9b19006a334970ed7e5462f94756b24151f406da
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-07-17 20:06:13 -0300

    lista 9.1 = atividade 3 ok

---
+++ turma-2/Jhouus33/Lista_9_1/atividade_3.js
@@ -0,0 +1,8 @@
+// 3 - (Obrigatório) Use o spread operator para transformar a string 'Array' em
+// ['A', 'r', 'r', 'a', 'y']
+
+const array = ["Array"]
+
+const arrayTransformada = [...array[0] ]
+
+console.log(arrayTransformada);
