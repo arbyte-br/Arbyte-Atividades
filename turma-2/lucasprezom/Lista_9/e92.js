58e39efbded15fff7bf2fdf8d747c5873dad44cf
lucasprezom <lucasprezom@gmail.com>
2020-07-17 21:28:33 -0300

    atualizando 9.1

---
+++ turma-2/lucasprezom/Lista_9/e92.js
@@ -0,0 +1,16 @@
+// 2 - (Obrigatório) Use o spread operator para transformar o array ​['​a​', '​b​', '​c​']​ 
+// em ['​a​', '​b​', '​c​', '​a​', '​b​', '​c​']
+
+const letras = ['a', 'b', 'c']
+const duplo = ['a','b','c',...letras]
+console.log (duplo)
+
+
+
+
+
+
+
+
+
+
