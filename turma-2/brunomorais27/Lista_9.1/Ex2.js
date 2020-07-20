6e94ddee449943ac1514973d0f1edaabffbab1ae
BrunoMorais27 <bru.morais15@gmail.com>
2020-07-20 00:11:22 -0300

    Lista 9.1 Finalizada

---
+++ turma-2/brunomorais27/Lista_9.1/Ex2.js
@@ -0,0 +1,3 @@
+const letras = ["a", "b", "c"]
+const resultado = [...letras, ...letras]
+console.log(resultado)  
---
+++ turma-2/brunomorais27/Lista_9.1/Ex4.js
@@ -0,0 +1,10 @@
+const objeto = {
+    a: 'a',
+    b: 'b',
+    c: 'c',
+}
+
+const d = 'd';
+const e = 'e'
+
+console.log({...objeto, d, e})
