864f38ed9fa1a1f58da9d60c62c7cc4dbf8a1e8b
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-07-20 20:48:46 -0300

    Lista 10 - atividade 1 ok

---
+++ turma-2/Jhouus33/Lista_10/atividade_1.js
@@ -0,0 +1,14 @@
+// Crie uma aplicação em JS que receba um nome do usuário e crie um
+// novo usuário com o nome fornecido e o imprima na tela(ex.: "{nome: 'Italo'}"). Este
+// objeto deve ser criado usando uma classe.
+
+let rs = require('readline-sync')
+
+class usuario {
+    constructor(nome){
+        this.nome = rs.question('Digite seu nome: ')
+    }
+
+}
+const novoUsuario = new usuario()
+console.log(novoUsuario)
