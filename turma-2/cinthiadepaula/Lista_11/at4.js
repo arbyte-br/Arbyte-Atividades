0b9593a57ea42f39f93587bca1e6b5986eee2267
Cinthia de Paula <66394231+cinthiadepaula@users.noreply.github.com>
2020-07-21 00:21:36 -0300

    Delete at4.js

--- turma-2/cinthiadepaula/Lista_11/at4.js
+++
@@ -1,7 +0,0 @@
-const readline = require('readline-sync');
-
-let graus = readline.question('Digite os Graus em Fahrenheint: ');
-
-const toCelsius = (graus) => ((5 / 9) * (graus - 32));
-
-console.log(`The Temperature is ${toCelsius(graus)} Celsius`);
