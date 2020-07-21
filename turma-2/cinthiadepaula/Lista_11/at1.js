1969c70f23b537ddd199d5348bc71bee99fa190a
Cinthia de Paula <66394231+cinthiadepaula@users.noreply.github.com>
2020-07-21 00:20:45 -0300

    Delete at1.js

--- turma-2/cinthiadepaula/Lista_11/at1.js
+++
@@ -1,9 +0,0 @@
-const rs = require('readline-sync');
-
-let a = rs.questionInt('Digite um numero: ');
-let b = rs.questionInt('Digite um numero: ');
-
-const soma = (a,b) => a+b;
-
-console.log(soma(a,b));
-
