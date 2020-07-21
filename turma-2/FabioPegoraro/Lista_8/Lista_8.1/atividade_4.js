51e0d92706556217476914ce7d5a5acd38ce3055
FabioPegoraro <66393999+FabioPegoraro@users.noreply.github.com>
2020-07-21 14:15:02 -0300

    Resultado Atividade 5 e 4

--- turma-2/FabioPegoraro/Lista_8/Lista_8.1/atividade_4.js
+++ turma-2/FabioPegoraro/Lista_8/Lista_8.1/atividade_4.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //a. Peça o login de um usuário; 
 //b. Peça sua senha; 
 //c. Caso o usuário e a senha estejam corretos, imprima na tela "USUÁRIO LOGADO"; 
@@ -20,29 +19,5 @@ let loginSenhaCorreta =  {
 if(usuario.login == loginSenhaCorreta.loginCerto && usuario.senha == loginSenhaCorreta.senhaCerta) {
     console.log("USUÁRIO LOGADO")
 }else{
-    console.log( "LOGIN/SENHA INCORRETOS") 
-=======
-//a. Peça o login de um usuário; 
-//b. Peça sua senha; 
-//c. Caso o usuário e a senha estejam corretos, imprima na tela "USUÁRIO LOGADO"; 
-//d. Caso usuário ou senha estejam incorretos, imprima na tela "LOGIN/SENHA INCORRETOS"; 
-//e. Pare a execução
-
-let rs = require("readline-sync")
-
-let usuario =  {
-    login: nomeUsuario = rs.question("Digite seu Usuario?"),
-    senha: senha = rs.questionInt("Digite a senha:")
-}
-
-let loginSenhaCorreta =  {
-    loginCerto: "fabio",
-    senhaCerta: "123456",
-}
-
-if(usuario.login == loginSenhaCorreta.loginCerto && usuario.senha == loginSenhaCorreta.senhaCerta) {
-    console.log("USUÁRIO LOGADO")
-}else{
-    console.log( "LOGIN/SENHA INCORRETOS") 
->>>>>>> 92cbf41a9853027554ba579418ec2147c36eb955
+    console.log( "LOGIN/SENHA INCORRETOS")
 }
