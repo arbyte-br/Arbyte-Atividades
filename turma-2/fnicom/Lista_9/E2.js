f0c4d654e3c09e421abca23fb3cfd198e5a4b2c9
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-07-20 21:06:09 -0300

    Add exercícios da lista 9.1
    Add exercícios da lista 9.1

---
+++ turma-2/fnicom/Lista_9/E2.js
@@ -0,0 +1,9 @@
+let rs = require('readline-sync'); 
+
+let array1 = ['a', 'b', 'c'];
+
+let array2 = ['a', 'b', 'c'];
+
+let resultado = [...array1, ...array2];
+
+console.log(resultado);
---
+++ turma-2/fnicom/Lista_9/E4.js
@@ -0,0 +1,2 @@
+let rs = require('readline-sync');
+
