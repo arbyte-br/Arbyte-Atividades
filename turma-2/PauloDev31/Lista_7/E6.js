35f6fa6a3baa3708233498ce6d05919e8f539604
PauloDev31 <mylifeispb@hotmail.com>
2020-07-21 13:29:01 -0300

    lista 7

---
+++ turma-2/PauloDev31/Lista_7/E6.js
@@ -0,0 +1,13 @@
+let rs = require('readline-sync')
+
+function numero(n1) {
+    
+    if (n1 > 0 && n1 <= 10){
+        return console.log('Número OK!')
+    }else {
+        return console.log('O número deve ser de 1 a 10!')
+    }
+}
+
+let n1 = rs.questionInt('Insira um numero entre 1 e 10: \n')
+numero(n1)
