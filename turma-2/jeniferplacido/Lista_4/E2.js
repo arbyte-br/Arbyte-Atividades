42ba948203ea5b59119dd1b27d1abf6e8821808f
Jenifer Placido <jenifer.sdti@gmail.com>
2020-07-30 16:26:48 -0300

    Lista4

---
+++ turma-2/jeniferplacido/Lista_4/E2.js
@@ -0,0 +1,36 @@
+// (OBRIGATÓRIO)​ Escreva um programa que passe por todos os números de 1 a 50 e
+//para cada um deles imprima na tela se o número é par ou ímpar. Caso o número
+//também seja primo, o programa deve dizer que ele é primo. (um número primo é um
+//número que só é divisível por ele mesmo e por 1). 
+//Dica: Neste exercício você podeprecisar de ajuda do operador MÓDULO (%). 
+//Você também pode precisar criar mais de um loop
+
+function numeroPrimo(numero) {
+    let contador = 2
+    let achouDivisorAlemDeleMesmoEUm = false
+    if (numero === 1) {
+        return false
+    }
+    while (contador < numero) {
+        if (numero % contador === 0) {
+            achouDivisorAlemDeleMesmoEUm = true
+        }
+        contador++
+    }
+    if (achouDivisorAlemDeleMesmoEUm) {
+        return false
+    } else {
+        return true
+    }
+}
+let contador = 1
+let fimContador = 50
+while (contador <= fimContador) {
+    let sobraDaDivisao = contador % 2
+    if (sobraDaDivisao === 0) {
+        console.log(contador, 'PAR', numeroPrimo(contador) ? 'É Primo' : 'Não é primo')
+    } else {
+        console.log(contador, 'IMPAR', numeroPrimo(contador) ? 'É Primo' : 'Não é primo')
+    }
+    contador++
+}
