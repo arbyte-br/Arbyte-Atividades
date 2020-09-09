0cd6c7384f0ab419dcbbfcd5771f6ad200ca0325
João Victor <victorps.joao@gmail.com>
2020-09-09 17:35:59 -0300

    Atividades de Jest e lista 15.2

--- turma-2/joaovictorps/Lista_15/15.2/src/index.js
+++ turma-2/joaovictorps/Lista_15/15.2/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const routes = require('./routes')
+const {swaggerConfig} = require('./middlewares/swagger')
+
 const port = 3000
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use(...swaggerConfig)
 
 app.get('/', (req, res) => {
     res.send('Conectado')
